339755e21301af22922758e0c35b87199b992d4d
Rex Attwood <rexattwood26@gmail.com>
2025-01-26 12:56:47 +0000

    Initial code to create a JFrame

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Walkers");
+        frame.setSize(1000, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
